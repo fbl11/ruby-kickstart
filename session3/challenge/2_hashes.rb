2f61876574675567ce9f45878707651722996372
freya <freyab_london@yahoo.co.uk>
2018-03-27 23:02:17 +0100

    Add some challenges Session 3 and Session 2

--- session3/challenge/2_hashes.rb
+++ session3/challenge/2_hashes.rb
@@ -8,3 +8,16 @@
 # staircase 4  # => {1 => [], 3 => [2]}
 # staircase 5  # => {1 => [], 3 => [2], 5 =>[2, 4]}
 
+def staircase(int)
+  return "Error" if int <= 0
+  # odds = (1..int).step(2).to_a
+  # evens = (2..int).step(2).to_a
+
+  (1..int).step(2).inject({}) { |hash, oddnumber| hash.merge({oddnumber => (2..oddnumber).step(2).to_a}) }
+  #
+  # odds.each do |oddnumber|
+  #   # hash[oddnumber] = evens.select { |evennumber| evennumber < oddnumber }
+  #   hash[oddnumber] = (2..oddnumber).step(2).to_a
+  # end
+  # hash
+end
